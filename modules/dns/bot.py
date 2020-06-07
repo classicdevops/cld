014404af0894dda496e88bf3b9d5a4da782d28e4
alexanderchendev <its1234586@gmail.com>
2020-06-07 16:52:44 +0500

    no message

--- modules/dns/bot.py
+++ modules/dns/bot.py
@@ -9,11 +9,27 @@ def cmd_getdns(message):
         dnscontent = ''
         dnscontent = str(dnsargs.group(2)).replace('None', '')
         cmdoutput = bash('/var/cld/modules/dns/bin/cld-getdns '+str(dnszone)+' '+str(dnscontent))
-        # print('/var/cld/modules/dns/bin/cld-getdns '+str(dnszone)+' '+str(dnscontent))
-        # print(cmdoutput)
         bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
      else:
         bot.send_message(message.chat.id, text="DNS zone or DNS content is not defined, please use format:\n`/getdns example.com 1.2.3.4`", parse_mode='Markdown')
+   else:
+      myid_answer = "user id is %s, access denied for %s" % (message.from_user.id, message.from_user.username)
+      bot.send_message(message.chat.id, myid_answer)
+
+@bot.message_handler(commands=["setdns"])
+def cmd_setdns(message):
+   valid_id = str(message.from_user.id)
+   if valid_id in allowusers():
+     if re.findall(r'(A|AAAA|CNAME|TXT|SRV|LOC|MX|NS|SPF|CERT|DNSKEY|DS|NAPTR|SMIMEA|SSHFP|TLSA|URI|a|aaaa|cname|txt|srv|loc|mx|ns|spf|cert|dnskey|ds|naptr|smimea|sshfp|tlsa|uri)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@-]+)', message.text):
+        dnsargs = re.search('(A|AAAA|CNAME|TXT|SRV|LOC|MX|NS|SPF|CERT|DNSKEY|DS|NAPTR|SMIMEA|SSHFP|TLSA|URI|a|aaaa|cname|txt|srv|loc|mx|ns|spf|cert|dnskey|ds|naptr|smimea|sshfp|tlsa|uri)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@/-]+)', message.text)
+        dnstype = dnsargs.group(1)
+        dnszone = dnsargs.group(2)
+        dnscontent = ''
+        dnscontent = str(dnsargs.group(3)).replace('None', '')
+        cmdoutput = bash('/var/cld/modules/dns/bin/cld-setdns '+str(dnstype)+' '+str(dnszone)+' '+str(dnscontent))
+        bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
+     else:
+        bot.send_message(message.chat.id, text="DNS type, zone or content is not defined, please use format:\n`/setdns example.com A 1.2.3.4`", parse_mode='Markdown')
    else:
       myid_answer = "user id is %s, access denied for %s" % (message.from_user.id, message.from_user.username)
       bot.send_message(message.chat.id, myid_answer)
