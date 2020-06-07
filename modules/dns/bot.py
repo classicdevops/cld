cab2822c915132c253c6b9fe044dda31c84f5cb7
alexanderchendev <its1234586@gmail.com>
2020-06-07 16:40:03 +0500

    no message

--- modules/dns/bot.py
+++ modules/dns/bot.py
@@ -7,11 +7,11 @@ def cmd_getdns(message):
         dnsargs = re.search('([a-z0-9.*-]+\.[a-z0-9.-]+)(\s+[A-Za-z0-9.*@-]+)?', message.text)
         dnszone = dnsargs.group(1)
         dnscontent = ''
-        dnscontent = dnsargs.group(2)
+        dnscontent = dnsargs.group(2).replace('None', '')
         cmdoutput = bash('/var/cld/modules/dns/bin/cld-getdns '+str(dnszone)+' '+str(dnscontent))
-        print('/var/cld/modules/dns/bin/cld-getdns '+str(dnszone)+' '+str(dnscontent))
-        print(cmdoutput)
-        # bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
+        # print('/var/cld/modules/dns/bin/cld-getdns '+str(dnszone)+' '+str(dnscontent))
+        # print(cmdoutput)
+        bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
      else:
         bot.send_message(message.chat.id, text="DNS zone or DNS content is not defined, please use format:\n`/getdns example.com 1.2.3.4`", parse_mode='Markdown')
    else:
