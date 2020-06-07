3bd8a6476e39f6a4d3884dddf3048f6dc253b2d1
alexanderchendev <its1234586@gmail.com>
2020-06-07 23:11:01 +0500

    no message

--- modules/access/bot.py
+++ modules/access/bot.py
@@ -7,11 +7,11 @@ def cmd_myip(message):
    if valid_id in allowgroups() or valid_id2 in allowusers():
      if re.findall(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', message.text):
         myip = re.search('([\d]+\.[\d]+\.[\d]+\.[\d]+)', message.text).group(1)
-        cmdoutput = subprocess.Popen('/var/cld/bot/bin/myip_add '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmdoutput = subprocess.Popen('/var/cld/modules/access/bin/myip_add '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         bot.send_message(message.chat.id, cmdoutput.communicate(), parse_mode='Markdown')
      else:
         myip = 'TOKEN'
-        cmdoutput = subprocess.Popen('/var/cld/bot/bin/myip_add '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmdoutput = subprocess.Popen('/var/cld/modules/access/bin/myip_add '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         bot.send_message(message.chat.id, cmdoutput.communicate(), parse_mode='Markdown', disable_web_page_preview='true')
    else:
       myid_answer = "user id is %s, access denied for %s" % (message.from_user.id, message.from_user.username)
@@ -26,7 +26,7 @@ def cmd_enableip(message):
         enableipargs = re.search('([\d]+\.[\d]+\.[\d]+\.[\d]+)\s+(.+)', message.text)
         enable_ip = enableipargs.group(1)
         enable_cmnt = enableipargs.group(2)
-        cmdoutput = subprocess.Popen('/var/cld/bot/bin/enableip_add '+str(enable_ip)+' '+str(enable_cmnt), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmdoutput = subprocess.Popen('/var/cld/modules/access/bin/enableip_add '+str(enable_ip)+' '+str(enable_cmnt), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         bot.send_message(message.chat.id, cmdoutput.communicate(), parse_mode='Markdown')
      else:
         bot.send_message(message.chat.id, text="ip address or comment is not defined, please use format:\n`/enableip 1.1.1.1 AccessReason`", parse_mode='Markdown')
