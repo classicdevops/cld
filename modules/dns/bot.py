9ae368e4ca75b201574cd654f9bfc812440d7dce
alexanderchendev <its1234586@gmail.com>
2020-06-07 16:24:26 +0500

    no message

--- modules/dns/bot.py
+++ modules/dns/bot.py
@@ -3,10 +3,10 @@
 def cmd_getdns(message):
    valid_id = str(message.from_user.id)
    if valid_id in allowusers():
-     if re.findall(r'([\d]+\.[\d]+\.[\d]+\.[\d]+)\s(.+)', message.text):
-        enableipargs = re.search('([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@-]+)', message.text)
-        dnszone = enableipargs.group(1)
-        dnscontent = enableipargs.group(2)
+     if re.findall(r'([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@-]+)', message.text):
+        dnsargs = re.search('([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@-]+)', message.text)
+        dnszone = dnsargs.group(1)
+        dnscontent = dnsargs.group(2)
         cmdoutput = bash('/var/cld/modules/dns/bin/cld-getdns '+str(enable_ip)+' '+str(enable_cmnt))
         bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
      else:
