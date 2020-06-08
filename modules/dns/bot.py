28c9bd4d3ea973f971962a7027bf0c36a59ec802
alexanderchendev <its1234586@gmail.com>
2020-06-08 16:58:22 +0500

    no message

--- modules/dns/bot.py
+++ modules/dns/bot.py
@@ -1,8 +1,11 @@
+def allowdnsgroups():
+  return set(bash("grep ALLOW_DNS_GROUP_IDS /var/cld/creds/creds_security_system | cut -d = -f 2").strip().split(','))
 
 @bot.message_handler(commands=["getdns"])
 def cmd_getdns(message):
-   valid_id = str(message.from_user.id)
-   if valid_id in allowusers():
+   valid_id = str(message.chat.id)
+   valid_id2 = str(message.from_user.id)
+   if valid_id in allowdnsgroups() or valid_id2 in allowusers():
      if re.findall(r'([a-z0-9.*-]+\.[a-z0-9.-]+)(\s+[A-Za-z0-9.*=@-]+)?', message.text):
         dnsargs = re.search('([a-z0-9.*-]+\.[a-z0-9.-]+)(\s+[A-Za-z0-9.*=@-]+)?', message.text)
         dnszone = dnsargs.group(1)
@@ -18,8 +21,9 @@ def cmd_getdns(message):
 
 @bot.message_handler(commands=["setdns"])
 def cmd_setdns(message):
-   valid_id = str(message.from_user.id)
-   if valid_id in allowusers():
+   valid_id = str(message.chat.id)
+   valid_id2 = str(message.from_user.id)
+   if valid_id in allowdnsgroups() or valid_id2 in allowusers():
      if re.findall(r'([a-zA-Z]+)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*=@/_-]+)', message.text):
         dnsargs = re.search('([a-zA-Z]+)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*=@/_-]+)', message.text)
         dnstype = dnsargs.group(1)
@@ -36,8 +40,9 @@ def cmd_setdns(message):
 
 @bot.message_handler(commands=["deldns"])
 def cmd_deldns(message):
-   valid_id = str(message.from_user.id)
-   if valid_id in allowusers():
+   valid_id = str(message.chat.id)
+   valid_id2 = str(message.from_user.id)
+   if valid_id in allowdnsgroups() or valid_id2 in allowusers():
      if re.findall(r'([a-zA-Z]+)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@/_-]+)', message.text):
         dnsargs = re.search('(A|AAAA|CNAME|TXT|SRV|LOC|MX|NS|SPF|CERT|DNSKEY|DS|NAPTR|SMIMEA|SSHFP|TLSA|URI|a|aaaa|cname|txt|srv|loc|mx|ns|spf|cert|dnskey|ds|naptr|smimea|sshfp|tlsa|uri)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@/_-]+)', message.text)
         dnstype = dnsargs.group(1)
@@ -54,8 +59,9 @@ def cmd_deldns(message):
 
 @bot.message_handler(commands=["getip"])
 def cmd_deldns(message):
-   valid_id = str(message.from_user.id)
-   if valid_id in allowusers():
+   valid_id = str(message.chat.id)
+   valid_id2 = str(message.from_user.id)
+   if valid_id in allowdnsgroups() or valid_id2 in allowusers():
      if re.findall(r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|all)', message.text):
         ipargs = re.search('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|all)', message.text)
         ipaddr = ipargs.group(1)
