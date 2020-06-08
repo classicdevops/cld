3453140896fc6589d07615356c8b109e365f43ff
alexanderchendev <its1234586@gmail.com>
2020-06-08 16:25:19 +0500

    no message

--- modules/dns/bot.py
+++ modules/dns/bot.py
@@ -51,3 +51,18 @@ def cmd_deldns(message):
    else:
       myid_answer = "user id is %s, access denied for %s" % (message.from_user.id, message.from_user.username)
       bot.send_message(message.chat.id, myid_answer)
+
+@bot.message_handler(commands=["getip"])
+def cmd_deldns(message):
+   valid_id = str(message.from_user.id)
+   if valid_id in allowusers():
+     if re.findall(r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|)', message.text):
+        ipargs = re.search('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|)', message.text)
+        ipaddr = dnsargs.group(1)
+        cmdoutput = bash('/var/cld/modules/dns/bin/cld-getip '+str(ipaddr))
+        bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
+     else:
+        bot.send_message(message.chat.id, text="DNS type, zone or content is not defined, please use format:\n`/deldns A example.com 1.2.3.4`", parse_mode='Markdown')
+   else:
+      myid_answer = "user id is %s, access denied for %s" % (message.from_user.id, message.from_user.username)
+      bot.send_message(message.chat.id, myid_answer)
