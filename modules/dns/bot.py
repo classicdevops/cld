d508eb967b84624c75559a2a33412127ae3052e4
alexanderchendev <its1234586@gmail.com>
2020-08-04 22:20:19 +0500

    no message

--- modules/dns/bot.py
+++ modules/dns/bot.py
@@ -63,7 +63,7 @@ def cmd_deldns(message):
       bot.send_message(message.chat.id, myid_answer)
 
 @bot.message_handler(commands=["getip"])
-def cmd_deldns(message):
+def cmd_getip(message):
    valid_id = str(message.chat.id)
    valid_id2 = str(message.from_user.id)
    if valid_id in allowdnsgroups() or valid_id2 in allowusers():
@@ -73,7 +73,23 @@ def cmd_deldns(message):
         cmdoutput = bash('/var/cld/modules/dns/bin/cld-getip '+str(ipaddr))
         bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
      else:
-        bot.send_message(message.chat.id, text="DNS type, zone or content is not defined, please use format:\n`/deldns A example.com 1.2.3.4`", parse_mode='Markdown')
+        bot.send_message(message.chat.id, text="DNS type, zone or content is not defined, please use format:\n`/getip 1.2.3.4`", parse_mode='Markdown')
+   else:
+      myid_answer = "user id is %s, access denied for %s" % (message.from_user.id, message.from_user.username)
+      bot.send_message(message.chat.id, myid_answer)
+
+
+@bot.message_handler(commands=["getwhois"])
+def cmd_getwhois(message):
+   valid_id = str(message.chat.id)
+   valid_id2 = str(message.from_user.id)
+   if valid_id in allowdnsgroups() or valid_id2 in allowusers():
+     if re.findall(r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|all)', message.text):
+        domainargs = re.search('([A-z0-9.-]+\.[A-z]+)', message.text)
+        cmdoutput = bash('/var/cld/modules/dns/bin/cld-getwhois '+str(domainargs.group(1)))
+        bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
+     else:
+        bot.send_message(message.chat.id, text="DNS zone or is not defined, please use format:\n`/getwhois example.com`", parse_mode='Markdown')
    else:
       myid_answer = "user id is %s, access denied for %s" % (message.from_user.id, message.from_user.username)
       bot.send_message(message.chat.id, myid_answer)
