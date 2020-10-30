986f7b5df978ad679ec077a7d4241e6c2f731a6f
alexanderchendev <its1234586@gmail.com>
2020-10-30 14:39:12 +0500

    no message

--- modules/dns/bot.py
+++ modules/dns/bot.py
@@ -24,7 +24,7 @@ def cmd_setdns(message):
    valid_id = str(message.chat.id)
    valid_id2 = str(message.from_user.id)
    if valid_id in allowdnsgroups() or valid_id2 in allowusers():
-     if re.findall(r'([a-zA-Z]+)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@/_-]+|[A-Za-z0-9.*=@/_-]+)', message.text):
+     if re.findall(r'([a-zA-Z]+)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*@/_ -]+|[A-Za-z0-9.*=@/_ -]+)', message.text):
         dnsargs = re.search('([a-zA-Z]+)\s+([a-z0-9.*-]+\.[a-z0-9.-]+)\s+([A-Za-z0-9.*=@/_-]+)(\s+[A-Za-z0-9.*=@/_-]+)?', message.text)
         dnstype = dnsargs.group(1)
         dnszone = dnsargs.group(2)
