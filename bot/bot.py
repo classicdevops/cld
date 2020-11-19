55ae7d19332f3a2aa7e4d0ad41e3437a3cf0373a
achendev <its1234586@gmail.com>
2020-11-19 17:07:40 +0500

    no message

--- bot/bot.py
+++ bot/bot.py
@@ -72,10 +72,10 @@ def allowmodulegroups(moduleperm):
   return set(bash('grep "'+moduleperm+'\|ALL" /var/cld/creds/tgbot_passwd | cut -d : -f 2 | grep "^-" | head -c -1 | tr "\n" ","').strip().split(','))
 
 def checkmoduleperms(moduleperm, chat_id, user_id, user_name):
-  chat_id=str(chat_id)
-  user_id=str(user_id)
-  if chat_id not in allowmodulegroups(moduleperm) and user_id not in allowmoduleusers(moduleperm):
-    bot.send_message(chat_id, str("user id is "+user_id+", access denied for "+user_name))
+  chat_id_str=str(chat_id)
+  user_id_str=str(user_id)
+  if chat_id_str not in allowmodulegroups(moduleperm) and user_id_str not in allowmoduleusers(moduleperm):
+    bot.send_message(chat_id_str, str("user id is "+user_id_str+", access denied for "+user_name))
     sys.exit(1)
 
 cldm={}
