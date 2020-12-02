bab3d5c9255b5010e2577a5cd0a024e4ed53e2f7
achendev <its1234586@gmail.com>
2020-12-02 20:30:41 +0500

    no message

--- modules/access/api.py
+++ modules/access/api.py
@@ -24,7 +24,7 @@ def myip():
 def banip():
   if remoteaddr() in accesslist:
     black_ip = re.search('([\d]+\.[\d]+\.[\d]+\.[\d]+)', request.args['ip']).group(1)
-    black_cmnt = re.search('([\d]+\.[\d]+\.[\d]+\.[\d]+)', request.args['comment']).group(1)
+    black_cmnt = re.search('([A-z0-9]]+)', request.args['comment']).group(1)
     cmdoutput = bash('/var/cld/modules/access/bin/blackip_add '+str(black_ip)+' '+str(black_cmnt))
     resp = Response(cmdoutput, status=200, mimetype='application/json')
     return resp
