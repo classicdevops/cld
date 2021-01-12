859592e6a3cdae71f128f0ce533b05860e471b43
unknown <its1234586@gmail.com>
2021-01-13 04:28:02 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -116,7 +116,7 @@ def connect():
         cmd = "TERM=xterm /bin/bash"
         print("child pid is", child_pid, flush=True)
         print(f"starting background task with command `{cmd}` to continously read and forward pty output to client")
-        socketio.start_background_task(read_and_forward_pty_output)
+        socketio.start_background_task(read_and_forward_pty_output, socketid)
         print("task started")
 
 
