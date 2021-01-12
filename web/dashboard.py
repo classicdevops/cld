afeb27a019ddad228c9332e315c25bbf9bdf4de6
unknown <its1234586@gmail.com>
2021-01-13 02:25:48 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -57,7 +57,7 @@ def remoteaddr():
 
 
 #socketio
-app.config["fd"] = None
+session["fd"] = None
 def set_winsize(fd, row, col, xpix=0, ypix=0):
     winsize = struct.pack("HHHH", row, col, xpix, ypix)
     fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
@@ -67,11 +67,11 @@ def read_and_forward_pty_output():
     max_read_bytes = 1024 * 20
     while True:
         socketio.sleep(0.01)
-        if app.config["fd"]:
+        if session["fd"]:
             timeout_sec = 0
-            (data_ready, _, _) = select.select([app.config["fd"]], [], [], timeout_sec)
+            (data_ready, _, _) = select.select([session["fd"]], [], [], timeout_sec)
             if data_ready:
-                output = os.read(app.config["fd"], max_read_bytes).decode()
+                output = os.read(session["fd"], max_read_bytes).decode()
                 socketio.emit("pty-output", {"output": output}, namespace="/pty")
 
 
@@ -80,50 +80,31 @@ def socket():
   if 'username' in session:
     return render_template("html/socket.html")
 
-
 @socketio.on("pty-input", namespace="/pty")
 def pty_input(data):
-    """write to the child pty. The pty sees this as if you are typing in a real
-    terminal.
-    """
-    if app.config["fd"]:
-        # print("writing to ptd: %s" % data["input"])
-        os.write(app.config["fd"], data["input"].encode())
-
+    if session["fd"]:
+        os.write(session["fd"], data["input"].encode())
 
 @socketio.on("resize", namespace="/pty")
 def resize(data):
-    if app.config["fd"]:
-        set_winsize(app.config["fd"], data["rows"], data["cols"])
-
+    if session["fd"]:
+        set_winsize(session["fd"], data["rows"], data["cols"])
 
 @socketio.on("connect", namespace="/pty")
 def connect():
-    """new client connected"""
-    #session['child_pid'] = ''
     if session['child_pid']:
-        # already started child process, don't start another
         return
-    # create child process attached to a pty we can read from and write to
     (child_pid, fd) = pty.fork()
     if child_pid == 0:
-        # this is the child process fork.
-        # anything printed here will show up in the pty, including the output
-        # of this subprocess
         session['child_pid'] = child_pid
         subprocess.run("TERM=xterm", "/bin/bash")
     else:
-        # this is the parent process fork.
-        # store child fd and pid
-        app.config["fd"] = fd
+        session["fd"] = fd
         session['child_pid'] = child_pid
         set_winsize(fd, 50, 50)
         cmd = "/bin/bash"
         print("child pid is", child_pid)
-        print(
-            f"starting background task with command `{cmd}` to continously read "
-            "and forward pty output to client"
-        )
+        print(f"starting background task with command `{cmd}` to continously read and forward pty output to client")
         socketio.start_background_task(target=read_and_forward_pty_output)
         print("task started")
 
