409bbdd0662a476af67e62cca2b979fd8b8220d1
unknown <its1234586@gmail.com>
2021-01-14 14:28:19 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -146,13 +146,13 @@ def connect():
       app.config["shell"]["child"+socketid] = child_pid
       subprocess.run("TERM=xterm /usr/bin/sudo -u "+user+" "+shellcmd, shell=True, executable='/bin/bash')
     else:
-      subprocpid = int(bash('ps axf -o pid,command | grep -v grep | grep -A1 "^'+str(child_pid)+' " | cut -d " " -f 1 | tail -1 | tr -d "\n"'))
-      app.config["shell"]["subprocpid"+socketid] = subprocpid
+      subprocpid = bash('ps axf -o pid,command | grep -v grep | grep -A1 "^'+str(child_pid)+' " | cut -d " " -f 1 | tail -1 | tr -d "\n"')
+      print("subprocpid is: "+str(subprocpid), flush=True)
+      app.config["shell"]["subprocpid"+socketid] = int(subprocpid)
       app.config["shell"][socketid] = fd
       app.config["shell"]["child"+socketid] = child_pid
-      print("subprocpid is: "+str(subprocpid), flush=True)
       set_winsize(fd, 50, 50)
-      socketio.start_background_task(read_and_forward_pty_output, socketid, fd, subprocpid, child_pid)
+      socketio.start_background_task(read_and_forward_pty_output, socketid, fd, int(subprocpid), child_pid)
       app.config["shell"]["run"+socketid] = "1"
 
 
