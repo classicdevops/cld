20383a18bd3c0fe939afa13251123961e8a5edd4
unknown <its1234586@gmail.com>
2021-01-14 05:06:01 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -99,10 +99,11 @@ def read_and_forward_pty_output(socketid, sessfd, subprocpid):
     max_read_bytes = 1024 * 20
     while True:
       socketio.sleep(0.01)
-#      if check_pid(subprocpid) != True:
-#          print("exit due child pid not exist", flush=True)
-#          return socketio.emit("pty-output", {"output"+socketid: "Process exited"}, namespace="/pty")
-      if sessfd:
+      if check_pid(subprocpid) != True:
+          print("exit due child pid not exist", flush=True)
+          socketio.emit("pty-output", {"output"+socketid: "Process exited"}, namespace="/pty")
+          sys.exit(0)
+      elif sessfd:
           timeout_sec = 0
           (data_ready, _, _) = select.select([sessfd], [], [], timeout_sec)
           if data_ready:
