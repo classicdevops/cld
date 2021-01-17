04a440dae28ba9410653b6f59ef511ffadb81289
unknown <its1234586@gmail.com>
2021-01-17 23:43:37 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -103,7 +103,7 @@ def tool(cldutility, args=None):
   if 'username' in session:
     user = session['username']
     cldfile = bash('''grep ' '''+cldutility+'''=' /home/'''+user+'''/.bashrc | cut -d "'" -f 2 | tr -d "\n"''')
-    print('''grep ' '''+cldutility+'''=' /home/'''+user+'''/.bashrc | cut -d "'" -f 2 | tr -d "\n"'''. flush=True)
+    print('''grep ' '''+cldutility+'''=' /home/'''+user+'''/.bashrc | cut -d "'" -f 2 | tr -d "\n"''', flush=True)
     cldmodule = bash('rev <<< '+cldfile+' | cut -d / -f 3 | rev | tr -d "\n"')
     checkresult = checkpermswhiteip(cldmodule, cldutility, user, remoteaddr()) 
     if checkresult[0] != "granted":
