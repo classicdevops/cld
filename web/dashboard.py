2175a298e5cf3989c91e8d2bd730f95e8a756279
unknown <its1234586@gmail.com>
2021-01-18 00:39:44 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -102,14 +102,15 @@ def set_winsize(fd, row, col, xpix=0, ypix=0):
 def tool(cldutility, args=None):
   if 'username' in session:
     user = session['username']
-    cldfile = bash('''grep ' '''+cldutility+'''=' /home/'''+user+'''/.bashrc | cut -d ' ' -f 3 | tr -d "'"''').replace('\n', '')
-    print('cldfile is: '+str(cldfile), flush=True)
-    if cldfile != '': cldmodule = bash('rev <<< '+cldfile+' | cut -d / -f 3 | rev | tr -d "\n"')
-    else: return Response("403", status=403, mimetype='application/json')
-    print('cldmodule is: '+str(cldmodule), flush=True)
-    checkresult = checkpermswhiteip(cldmodule, cldutility, user, remoteaddr())
-    print('checkresult is: '+str(checkresult), flush=True)
-    if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
+    if cldutility != 'bash':
+      cldfile = bash('''grep ' '''+cldutility+'''=' /home/'''+user+'''/.bashrc | cut -d ' ' -f 3 | tr -d "'"''').replace('\n', '')
+      print('cldfile is: '+str(cldfile), flush=True)
+      if cldfile != '': cldmodule = bash('rev <<< '+cldfile+' | cut -d / -f 3 | rev | tr -d "\n"')
+      else: return Response("403", status=403, mimetype='application/json')
+      print('cldmodule is: '+str(cldmodule), flush=True)
+      checkresult = checkpermswhiteip(cldmodule, cldutility, user, remoteaddr())
+      print('checkresult is: '+str(checkresult), flush=True)
+      if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
     try: cmd_args = str(re.match('^[A-z0-9.,@=/ -]+$', args).string)
     except: cmd_args = ''
     try: cmd_args = str(re.match('^[A-z0-9.,@=/ -]+$', request.args['args']).string)
