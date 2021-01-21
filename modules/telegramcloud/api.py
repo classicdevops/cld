2519ccbd4cd73b0f5008b20c380535d11fc3217b
unknown <its1234586@gmail.com>
2021-01-21 18:56:28 +0500

    no message

--- modules/telegramcloud/api.py
+++ modules/telegramcloud/api.py
@@ -1,5 +1,4 @@
-from requests import get
 @app.route('/tcloud/<telegram_file_id>/<file_name>')
 def telegramclouddownload(telegram_file_id, file_name):
-	tg_file_path = json.loads(get("https://api.telegram.org/bot"+telegram_bot_token+"/getFile?file_id="+telegram_file_id).content)['result']['file_path']
-	return get("https://api.telegram.org/file/bot"+tgtoken+"/"+tg_file_path).content
+	tg_file_path = json.loads(requests.get("https://api.telegram.org/bot"+telegram_bot_token+"/getFile?file_id="+telegram_file_id).content)['result']['file_path']
+	return requests.get("https://api.telegram.org/file/bot"+telegram_bot_token+"/"+tg_file_path).content
