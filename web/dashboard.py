879efc5b3fa9cf872b54bcf7e564111c16471758
unknown <its1234586@gmail.com>
2021-01-21 22:27:34 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -23,6 +23,8 @@ import struct
 import fcntl
 import shlex
 import threading
+import eventlet
+eventlet.monkey_patch()
 
 def bash(cmd):
   return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, executable='/bin/bash').communicate()[0].decode('utf8')
