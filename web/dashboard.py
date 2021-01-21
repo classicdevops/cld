34b9e4b29d109bf0ee6429f3f257a91f4e33d7de
unknown <its1234586@gmail.com>
2021-01-21 21:14:05 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -167,8 +167,8 @@ def keepalive_shell_session(socketid, child_pid, room, subprocpid, fd, sid):
                 del app.config["shell"]["subprocpid"+socketid]
               except:
                 pass
-              #leave_room("room"+socketid, sid=sid, namespace='/cld')
-              #except: print("leave room exception - sid: "+str(sid), flush=True)
+              try: leave_room(room="room"+socketid, sid=sid, namespace='/cld')
+              except: print("leave room exception - sid: "+str(sid), flush=True)
               try: close_room("room"+socketid, '/cld')
               except: print("close_room room exception - sid :"+str(sid), flush=True)
               try: disconnect(sid, '/cld')
