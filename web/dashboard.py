a18c727ee5519e489b779e7e3159b9aa886c3156
unknown <its1234586@gmail.com>
2021-01-21 23:16:32 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -161,7 +161,7 @@ def keepalive_shell_session(socketid, child_pid, room, subprocpid, fd):
               print("started terminating task for socket "+socketid, flush=True)
               print("exit due "+socketid+" not conencted", flush=True)
               socketio.emit("output", {"output": "Process exited"}, namespace="/cld", room=room)
-              #socketio.emit("disconnect", namespace="/cld", room=room)
+              socketio.emit("disconnect", namespace="/cld", room=room)
               if check_pid(subprocpid) == True:
                 bash('kill -9 '+str(subprocpid))
                 time.sleep(1)
@@ -261,7 +261,7 @@ def connect():
       set_winsize(fd, 50, 50)
       socketio.start_background_task(read_and_forward_pty_output, socketid, fd, int(subprocpid), child_pid, room)
       print(str(socketid), str(fd), str(subprocpid), str(child_pid), str(room), flush=True)
-      threading.Thread(target=keepalive_shell_session, args=(socketid, child_pid, room, int(subprocpid), fd)).start()
+      #threading.Thread(target=keepalive_shell_session, args=(socketid, child_pid, room, int(subprocpid), fd)).start()
 
 #@app.after_request
 
