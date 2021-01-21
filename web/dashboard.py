1b1b679b9a22a5f440bc4c038bfa98b36bb429f0
unknown <its1234586@gmail.com>
2021-01-21 23:32:12 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -160,8 +160,10 @@ def keepalive_shell_session(socketid, child_pid, room, subprocpid, fd):
           if current_timestamp > socket_timestamp:
               print("started terminating task for socket "+socketid, flush=True)
               print("exit due "+socketid+" not conencted", flush=True)
-              socketio.emit("output", {"output": "Process exited"}, namespace="/cld", room=room)
-              socketio.emit("disconnect", namespace="/cld", room=room)
+              socketio.emit("output", {"output": "Process exited"}, namespace="/cld", room=room, sid=socketid)
+              socketio.emit("disconnect", namespace="/cld", room=room, sid=socketid)
+              socketio.close_room(room)
+              socketio.disconnect(namespace="/cld", sid=socketid)
               if check_pid(subprocpid) == True:
                 bash('kill -9 '+str(subprocpid))
                 time.sleep(1)
@@ -174,7 +176,7 @@ def read_and_forward_pty_output(socketid, sessfd, subprocpid, child_pid, room):
     while True:
       socketio.sleep(0.05)
       if check_pid(subprocpid) != True:
-          #print("exit due child pid not exist", flush=True)
+          print("exit due child pid not exist", flush=True)
           socketio.emit("output", {"output": "Process exited"}, namespace="/cld", room=room, sid=socketid)
           socketio.emit("disconnect", namespace="/cld", room=room, sid=socketid)
           return
@@ -261,7 +263,7 @@ def connect():
       set_winsize(fd, 50, 50)
       socketio.start_background_task(read_and_forward_pty_output, socketid, fd, int(subprocpid), child_pid, room)
       print(str(socketid), str(fd), str(subprocpid), str(child_pid), str(room), flush=True)
-      #threading.Thread(target=keepalive_shell_session, args=(socketid, child_pid, room, int(subprocpid), fd)).start()
+      threading.Thread(target=keepalive_shell_session, args=(socketid, child_pid, room, int(subprocpid), fd)).start()
 
 #@app.after_request
 
