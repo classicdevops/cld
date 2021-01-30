aab8867bf42a9925dd6f2379d01d91e950151936
unknown <its1234586@gmail.com>
2021-01-31 02:10:40 +0500

    no message

--- modules/access/bot.py
+++ modules/access/bot.py
@@ -10,6 +10,13 @@ def cmd_myip(message):
   else:
     myip = 'TOKEN'
     cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/myip '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip))
-    last_message = bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
-    sleep(10)
+        bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="White list my ip", url=cmdoutput)
+    keyboard.add(url_button)
+    last_message = bot.send_message(message.chat.id, 'Button TTL 10s', reply_markup=keyboard)
+    sleep(5)
     bot.delete_message(last_message.chat.id, last_message.message_id)
+    # last_message = bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
+    # sleep(10)
+    # bot.delete_message(last_message.chat.id, last_message.message_id)
