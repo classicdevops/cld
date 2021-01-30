8237a2da9a3b689ed8d65e632833a51b15799d77
unknown <its1234586@gmail.com>
2021-01-31 02:13:01 +0500

    no message

--- modules/access/bot.py
+++ modules/access/bot.py
@@ -15,7 +15,7 @@ def cmd_myip(message):
     url_button = types.InlineKeyboardButton(text="White list my ip", url=cmdoutput)
     keyboard.add(url_button)
     last_message = bot.send_message(message.chat.id, 'Button TTL 10s', reply_markup=keyboard)
-    sleep(5)
+    sleep(10)
     bot.delete_message(last_message.chat.id, last_message.message_id)
     # last_message = bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
     # sleep(10)
