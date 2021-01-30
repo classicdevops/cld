260aa15add1e3ed24540c8f23f536f87ff918554
unknown <its1234586@gmail.com>
2021-01-30 17:30:03 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -23,7 +23,6 @@ import struct
 import fcntl
 import shlex
 import threading
-import greenlet
 
 def bash(cmd):
   return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, executable='/bin/bash').communicate()[0].decode('utf8')
@@ -82,7 +81,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 DOCKERHOST = bash('grep DOCKERHOST /var/cld/creds/creds | cut -d = -f 2').replace('\n', '')
 
 app = Flask(__name__, template_folder=template_dir)
-socketio = SocketIO(app, cors_allowed_origins='https://dev-panel.classicdeploy.com', threading=greenlet)
+socketio = SocketIO(app, cors_allowed_origins='https://dev-panel.classicdeploy.com', threading=threading)
 app.config['UPLOAD_FOLDER'] = upload_dir
 #app.secret_key = FLASKSECRETKEY
 app.config['SESSION_TYPE'] = 'filesystem'
