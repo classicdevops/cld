3ddfee54954784275c573d9e09ded2665258a7ca
unknown <its1234586@gmail.com>
2021-01-31 01:55:02 +0500

    no message

--- modules/access/bot.py
+++ modules/access/bot.py
@@ -10,14 +10,4 @@ def cmd_myip(message):
   else:
     myip = 'TOKEN'
     cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/myip '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip))
-    # bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
-    keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="White list my ip", url=cmdoutput, callback_data="myip")
-    keyboard.add(url_button)
-    bot.send_message(message.chat.id, 'Link:', reply_markup=keyboard)
-
-@bot.callback_query_handler(lambda query: query.data == "myip")
-def myip_callback(query):
-    chat_id=query.message.chat.id
-    message_id=query.message.message_id
-    bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.message_id, text="Link expired")
+    bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
