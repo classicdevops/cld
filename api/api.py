2c026b465f0eff80f3256f34005df5723420121f
unknown <its1234586@gmail.com>
2021-02-02 01:37:39 +0500

    no message

--- api/api.py
+++ api/api.py
@@ -16,7 +16,7 @@ def bash(cmd):
 
 def bashstream(cmd):
   process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, executable='/bin/bash')
-  for line in process.stdout: yield ''.join(bash('echo -e '+line.decode('utf8')+' | /usr/bin/aha -n'))
+  for line in process.stdout: yield ''.join(bash('echo -e "'+line.decode('utf8')+'" | /usr/bin/aha'))
 
 telegram_bot_token = bash('''grep TELEGRAM_BOT_TOKEN /var/cld/creds/creds | cut -d = -f 2 | tr -d '"' | head -c -1''')
 
