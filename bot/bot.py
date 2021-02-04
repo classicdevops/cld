c604b18f6c34a47c420e1b760eaf0445ebae00ee
unknown <its1234586@gmail.com>
2021-02-04 21:07:25 +0500

    no message

--- bot/bot.py
+++ bot/bot.py
@@ -22,10 +22,11 @@ api_domain = bash('''grep CLD_DOMAIN /var/cld/creds/creds | cut -d = -f 2 | tr -
 @bot.message_handler(commands=["passwd"])
 def cmd_passwd(message):
     passlen = 21
+    cmd_args = ''
     try:
         for arg in message.text.split()[1:]: cmd_args=cmd_args+" "+str(arg)
         print(cmd_args, flush=True)
-        cmd_args = int(re.match('[0-9]{1,3}', cmd_args).string)
+        cmd_args = str(re.match('[0-9]{1,3}', cmd_args).string)
         passlen = cmd_args
     except:
         pass
