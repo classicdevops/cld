2772aa6383182903af7689b0234d4d523400ada2
unknown <its1234586@gmail.com>
2021-02-04 21:08:32 +0500

    no message

--- bot/bot.py
+++ bot/bot.py
@@ -26,7 +26,8 @@ def cmd_passwd(message):
     try:
         for arg in message.text.split()[1:]: cmd_args=cmd_args+" "+str(arg)
         print(cmd_args, flush=True)
-        cmd_args = str(re.match('[0-9]{1,3}', cmd_args).string)
+        cmd_args = int(re.match('[0-9]{1,3}', cmd_args).string)
+        print(cmd_args, flush=True)
         passlen = cmd_args
     except:
         pass
