817a92ec3716f9a7372331107b5f8cd7a0876538
unknown <its1234586@gmail.com>
2021-02-08 02:19:32 +0500

    no message

--- modules/access/api.py
+++ modules/access/api.py
@@ -2,7 +2,7 @@
 def myip():
   if 'token' in request.args:
     token = re.fullmatch(r'[a-z0-9]+', request.args['token']).string
-    output = bash('FROM=API /var/cld/modules/access/bin/activate_token '+str(remoteaddr())+' '+token)
+    output = bash('FROM=API /var/cld/modules/access/bin/cld-activateapitemptoken '+str(remoteaddr())+' '+token)
     resp = Response(output, status=200, mimetype='application/json')
     return resp
   else:
