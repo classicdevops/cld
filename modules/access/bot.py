2d05631d49a0b5a573d551dd86f4c3a0dc0656a9
unknown <its1234586@gmail.com>
2021-02-08 02:41:36 +0500

    no message

--- modules/access/bot.py
+++ modules/access/bot.py
@@ -5,13 +5,13 @@ def cmd_myip(message):
   user = bash('grep ":'+checkresult[1]+':" /var/cld/creds/passwd | cut -d : -f 1 | head -1 | tr -d "\\n"')
   if re.findall(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', message.text):
     myip = re.search('([\d]+\.[\d]+\.[\d]+\.[\d]+)', message.text).group(1)
-    cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/cld-myip '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip))
+    cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/cld-myipbot '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip))
     last_message = bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown')
     sleep(10)
     bot.delete_message(last_message.chat.id, last_message.message_id)
   else:
     myip = 'TOKEN'
-    cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/cld-myip '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip))
+    cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/cld-myipbot '+str(message.from_user.id)+' '+str(message.from_user.username)+' '+str(myip))
     last_message = bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
     sleep(10)
     bot.delete_message(last_message.chat.id, last_message.message_id)
