be68395da771f6c6d59d572ce3c60aff2526dd9f
unknown <its1234586@gmail.com>
2021-02-09 03:43:31 +0500

    no message

--- cm/web.py
+++ cm/web.py
@@ -4,4 +4,4 @@ def cm_index():
     user = session['username']
     checkresult = checkpermswhiteip(cldmodule, 'NOTUTILITY', user, remoteaddr())
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
-    return render_template("modules/cm/web/index.html")
+    return render_template("modules/cm/index.html")
