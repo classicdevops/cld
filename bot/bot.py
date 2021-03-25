52913d1a8a6983e9c688dc0e05f6b82288c5a7e9
unknown <its1234586@gmail.com>
2021-03-26 01:40:30 +0500

    no message

--- bot/bot.py
+++ bot/bot.py
@@ -22,12 +22,12 @@ def bot_bash_stream(cmd, message):
        cmdid += random.choice(chars)
     with subprocess.Popen(cmd+' | tee -a /var/cld/tmp/bot_cmd_'+cmdid+'; touch /var/cld/tmp/bot_cmd_'+cmdid+'_end', shell=True, stdout=subprocess.PIPE, universal_newlines=True, executable='/bin/bash') as p:
         while p.stdout:
-            try: bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+'```', parse_mode='Markdown')
+            try: bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+os.linesep+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+os.linesep+'```', parse_mode='Markdown')
             except: pass
             sleep(0.5)
             if os.path.exists('/var/cld/tmp/bot_cmd_'+cmdid+'_end'):
                 break
-    try: bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+'```', parse_mode='Markdown')
+    try: bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+os.linesep+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+os.linesep+'```', parse_mode='Markdown')
     except: pass
     return bash('rm -f /var/cld/tmp/bot_cmd_'+cmdid+' /var/cld/tmp/bot_cmd_'+cmdid+'_end')
 
