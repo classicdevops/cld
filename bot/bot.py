77bd2fbb4acaf086de58ecf2ac74ecfc6de8cc41
unknown <its1234586@gmail.com>
2021-03-26 01:45:12 +0500

    no message

--- bot/bot.py
+++ bot/bot.py
@@ -26,8 +26,8 @@ def bot_bash_stream(cmd, message):
             except: pass
             sleep(1)
             if os.path.exists('/var/cld/tmp/bot_cmd_'+cmdid+'_end'):
-                break
-    bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+os.linesep+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+os.linesep+'```', parse_mode='Markdown')
+                bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+os.linesep+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+os.linesep+'```', parse_mode='Markdown')
+                return bash('rm -f /var/cld/tmp/bot_cmd_'+cmdid+' /var/cld/tmp/bot_cmd_'+cmdid+'_end')
     return bash('rm -f /var/cld/tmp/bot_cmd_'+cmdid+' /var/cld/tmp/bot_cmd_'+cmdid+'_end')
 
 def arg(arg, message):
