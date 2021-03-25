fe6447b7c3fc0697c5f93c74399917a13a791cf7
unknown <its1234586@gmail.com>
2021-03-26 01:42:49 +0500

    no message

--- bot/bot.py
+++ bot/bot.py
@@ -27,8 +27,7 @@ def bot_bash_stream(cmd, message):
             sleep(0.5)
             if os.path.exists('/var/cld/tmp/bot_cmd_'+cmdid+'_end'):
                 break
-    try: bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+os.linesep+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+os.linesep+'```', parse_mode='Markdown')
-    except: pass
+    bot.edit_message_text(chat_id=openmessage.chat.id, message_id=openmessage.message_id, text='```'+os.linesep+bash('tail -20 /var/cld/tmp/bot_cmd_'+cmdid)+os.linesep+'```', parse_mode='Markdown')
     return bash('rm -f /var/cld/tmp/bot_cmd_'+cmdid+' /var/cld/tmp/bot_cmd_'+cmdid+'_end')
 
 def arg(arg, message):
