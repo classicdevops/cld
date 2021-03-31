876cf757ca0e8f3f5621a381d7f4fe575beb1e77
unknown <its1234586@gmail.com>
2021-04-01 01:35:50 +0500

    no message

--- deploy/web.py
+++ deploy/web.py
@@ -9,3 +9,19 @@ def deploy_index():
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
     deploys = json.loads(bash('sudo -u '+user+' sudo FROM=CLI /var/cld/deploy/bin/cld-deploy --list --json'))
     return render_template('modules/deploy/deploy.html', username=user, deploys=deploys)
+
+@app.route("/deploy/templates/<template>/<file>")
+def deploy_template_file(template, file):
+  if 'username' in session:
+    user = session['username']
+    checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
+    if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
+    return bash('cat /var/cld/deploy/templates/'+template+'/'+file)
+
+@app.route("/deploy/deploys/<deploy>/<file>")
+def deploy_deploy_file(deploy, file):
+  if 'username' in session:
+    user = session['username']
+    checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
+    if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
+    return bash('cat /var/cld/deploy/deploys/'+deploy+'/'+file)
