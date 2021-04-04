407db90fff3efa5bdb536b7830c71dccbcb8a949
unknown <its1234586@gmail.com>
2021-04-04 15:16:12 +0500

    no message

--- deploy/web.py
+++ deploy/web.py
@@ -34,6 +34,25 @@ def deploy_deploy_file(deploytype, deploy, file):
     else:
         return Response(deploytype[:-1].capitalize()+" not found", status=404, mimetype='text/plain')
 
+@app.route("/deploy/<deploytype>/<deploy>/files")
+def deploy_deploy_file(deploytype, deploy, file):
+  if 'username' in session:
+    user = session['username']
+    checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
+    if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
+    user_allowed_deploys = json.loads(bash('sudo -u '+user+' sudo FROM=CLI /var/cld/deploy/bin/cld-deploy --list --json'))
+    if deploytype == "templates":
+        deploys = user_allowed_deploys[0]['content']
+    elif deploytype == "deploys":
+        deploys = user_allowed_deploys[1]['content']
+    if deploy in deploys:
+        deploy_file_list = bash('{ DEPLOY_FILES=$(ls /var/cld/deploy/'+deploytype+'/'+deploy+') ; for CLD_FILE in vars script clouds ; do grep -s "^$CLD_FILE" <<< "$DEPLOY_FILES" ; done ; grep -v "^vars\|^script\|^clouds" <<< "$DEPLOY_FILES" ; }').split('\n')
+        deploy_file_dict = {}
+        for deploy_file in deploy_file_list:
+            deploy_file_dict[deploy_file] = bash('cat /var/cld/deploy/'+deploytype+'/'+deploy+'/'+deploy_file)
+        return Response(json.dumps(deploy_file_dict), status=200, mimetype='application/json')
+
+
 @app.route("/deploy/delete/<deploytype>/<deploy>")
 def deploy_delete(deploytype, deploy):
   if 'username' in session:
