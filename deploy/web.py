8dac4d898caf475e5b42eb6e608e2fe19910d16b
unknown <its1234586@gmail.com>
2021-04-04 15:06:13 +0500

    no message

--- deploy/web.py
+++ deploy/web.py
@@ -18,21 +18,21 @@ def deploy_files(deploytype, deploy):
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
     return Response(json.dumps(bash('{ DEPLOY_FILES=$(ls /var/cld/deploy/'+deploytype+'/'+deploy+') ; for CLD_FILE in vars script clouds ; do grep -s "^$CLD_FILE" <<< "$DEPLOY_FILES" ; done ; grep -v "^vars\|^script\|^clouds" <<< "$DEPLOY_FILES" ; }').split('\n')), status=200, mimetype='application/json')
 
-@app.route("/deploy/templates/<template>/<file>")
-def deploy_template_file(template, file):
+@app.route("/deploy/<deploytype>/<deploy>/<file>")
+def deploy_deploy_file(deploytype, deploy, file):
   if 'username' in session:
     user = session['username']
     checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
-    return Response(bash('cat /var/cld/deploy/templates/'+template+'/'+file), status=200, mimetype='text/plain')
-
-@app.route("/deploy/deploys/<deploy>/<file>")
-def deploy_deploy_file(deploy, file):
-  if 'username' in session:
-    user = session['username']
-    checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
-    if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
-    return Response(bash('cat /var/cld/deploy/deploys/'+deploy+'/'+file), status=200, mimetype='text/plain')
+    user_allowed_deploys = json.loads(bash('sudo -u '+user+' sudo FROM=CLI /var/cld/deploy/bin/cld-deploy --list --json'))
+    if deploytype == "templates":
+        deploys = user_allowed_deploys[0]['content']
+    elif deploytype == "deploys":
+        deploys = user_allowed_deploys[1]['content']
+    if deploy in deploys:
+        return Response(bash('cat /var/cld/deploy/'+deploytype+'/'+deploy+'/'+file), status=200, mimetype='text/plain')
+    else:
+        return Response(deploytype[:-1].capitalize()+" not found", status=404, mimetype='text/plain')
 
 @app.route("/deploy/delete/<deploytype>/<deploy>")
 def deploy_delete(deploytype, deploy):
