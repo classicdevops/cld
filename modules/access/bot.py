5dbbc03f5a74572fa61df8eadd7fdf32826ab8a4
unknown <its1234586@gmail.com>
2021-04-08 13:07:54 +0500

    no message

--- modules/access/bot.py
+++ modules/access/bot.py
@@ -1,6 +1,6 @@
 @bot.message_handler(commands=["myip"])
 def cmd_myip(message):
-  checkresult = checkperms(cldmodule, "myip", message.from_user.id, message.chat.id, message.from_user.username)
+  checkresult = checkperms(cldmodule, "cld-myipbot", message.from_user.id, message.chat.id, message.from_user.username)
   if checkresult[0] != "granted": return
   user = bash('grep ":'+checkresult[1]+':" /var/cld/creds/passwd | cut -d : -f 1 | head -1 | tr -d "\\n"')
   if re.findall(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', message.text):
