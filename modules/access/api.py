6f142330e910a150d87b13fae1b6b5216c760a0a
unknown <its1234586@gmail.com>
2021-04-13 13:22:36 +0500

    no message

--- modules/access/api.py
+++ modules/access/api.py
@@ -3,7 +3,7 @@ def myip():
   if 'token' in request.args:
     token = re.fullmatch(r'[a-z0-9]+', request.args['token']).string
     output = bash('FROM=API /var/cld/modules/access/bin/cld-activateapitemptoken '+str(remoteaddr())+' '+token)
-    resp = Response(output, status=200, mimetype='application/json')
+    resp = Response(output, status=200, mimetype='text/plain')
     return resp
   else:
     return 403
