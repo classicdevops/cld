85767fd7449f7a9d8657838f66d4016f81782db6
unknown <its1234586@gmail.com>
2021-04-23 02:33:24 +0500

    no message

--- modules/note/api.py
+++ modules/note/api.py
@@ -14,7 +14,7 @@ def note_get_file(note, file):
     checkresult = checkpermswhiteip(cldmodule, "NONE", token,  remoteaddr())
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='text/plain')
     user = userbytoken(checkresult[1])
-    if os.path.isfile('/var/cld/modules/note/date/'+note+'/'+file):
-        return Response(open('/var/cld/modules/note/date/'+note+'/'+file).read(), status=200, mimetype='text/plain')
+    if os.path.isfile('/var/cld/modules/note/data/'+note+'/'+file):
+        return Response(open('/var/cld/modules/note/data/'+note+'/'+file).read(), status=200, mimetype='text/plain')
     else:
         return Response(note.capitalize()+" not found", status=404, mimetype='text/plain')
