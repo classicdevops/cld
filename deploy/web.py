3a5f4c0ada2576199ef03f86c3fa71c71f45c38c
unknown <its1234586@gmail.com>
2021-04-26 23:40:26 +0500

    no message

--- deploy/web.py
+++ deploy/web.py
@@ -81,7 +81,7 @@ def deploy_save(deploytype, deploy):
     if deploy in deploys:
         deployfiles = dict(request.form)
         for deployfile in deployfiles:
-            open("/var/cld/deploy/"+deploytype+"/"+deploy+"/"+deployfile, "w", newline='\n').write(deployfiles[deployfile].replace('\r', ''))
+            open("/var/cld/deploy/"+vld(deploytype)+"/"+vld(deploy)+"/"+vld(deployfile), "w", newline='\n').write(deployfiles[deployfile].replace('\r', ''))
         return Response(deploytype[:-1].capitalize()+" saved", status=200, mimetype='text/plain')
     else:
         return Response(deploytype[:-1].capitalize()+" not found", status=404, mimetype='text/plain')
