a4e9af54f7b3df7e26df1197aa662059ec4f57ed
unknown <its1234586@gmail.com>
2021-04-27 01:12:42 +0500

    no message

---
+++ modules/access/web.py
@@ -0,0 +1,19 @@
+webmodule["access"] = {}
+webmodule["access"]["homename"] = "Access"
+webmodule["access"]["desc"] = "Classic firewall"
+@app.route("/access")
+def access_index():
+  if 'username' in session:
+    if userisadmin(session['username']) != True:
+      session.pop('username', None)
+      return redirect('/', code=302)
+    user = session['username']
+    checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
+    if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
+	file_list = ['/var/cld/modules/access/data/myips', '/var/cld/modules/access/data/enabledips', '/var/cld/modules/access/data/blackips', '/var/cld/modules/access/data/myip_tokens']
+    files = {}
+    for file in file_list:
+      if os.path.exists(file) != True:
+        bash('touch '+vld(file))
+      files[file] = open(file).read()
+    return render_template('modules/access/access.html', username=user, files=files)
