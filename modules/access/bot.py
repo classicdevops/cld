ab25f2150b6a99b509a97f6b02a64557234b1b9a
unknown <its1234586@gmail.com>
2021-05-24 02:42:33 +0500

    no message

--- modules/access/bot.py
+++ modules/access/bot.py
@@ -23,7 +23,12 @@ def cmd_myvpn(message):
   checkresult = checkperms(cldmodule, "cld-myvpnbot", message.from_user.id, message.chat.id, message.from_user.username)
   if checkresult[0] != "granted": return
   user = bash('grep ":'+checkresult[1]+':" /var/cld/creds/passwd | cut -d : -f 1 | head -1 | tr -d "\\n"')
-  cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/cld-myvpnbot '+str(message.from_user.id)+' '+str(message.from_user.username))
+  try:
+      for arg in message.text.split()[1:]: cmd_args=cmd_args+" "+str(arg)
+      cmd_args = str(re.match('^[A-z0-9.,@=/ -]+\$', cmd_args).string)
+  except:
+      pass
+  cmdoutput = bash('sudo -u '+user+' sudo FROM=BOT /var/cld/modules/access/bin/cld-myvpnbot '+vld(message.from_user.id)+' '+vld(message.from_user.username)+' '+cmd_args)
   last_message = bot.send_message(message.chat.id, cmdoutput, parse_mode='Markdown', disable_web_page_preview='true')
   open("/var/cld/modules/access/data/myvpn_token_chats", "a").write(cmdoutput.split('=')[1]+"_"+str(last_message.message_id)+"_"+str(message.chat.id)+"\n")
   sleep(10)
