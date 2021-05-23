17efacebf13ee9f060df0829cff46fd19eb61b55
unknown <its1234586@gmail.com>
2021-05-24 00:05:44 +0500

    no message

--- modules/access/api.py
+++ modules/access/api.py
@@ -1,7 +1,7 @@
 @app.route('/all/myipinit')
 def myip():
   if 'token' in request.args:
-    token = re.fullmatch(r'[a-z0-9]+', request.args['token']).string
+    token = re.fullmatch(r'[A-Za-z0-9]+', request.args['token']).string
     output = bash('FROM=API /var/cld/modules/access/bin/cld-activateiptoken '+str(remoteaddr())+' '+token)
     resp = Response(output, status=200, mimetype='text/plain')
     return resp
@@ -11,7 +11,7 @@ def myip():
 @app.route('/myvpnget')
 def myvpn():
   if 'token' in request.args:
-    token = re.fullmatch(r'[a-z0-9]+', request.args['token']).string
+    token = re.fullmatch(r'[A-Za-z0-9]+', request.args['token']).string
     file_path = bash('FROM=API /var/cld/modules/access/bin/cld-activatevpntoken '+token)
     if os.path.exists(file_path) != True:
       return Response('404', status=404, mimetype='text/plain')
