299b0b9801cdf5279ff2c588d052f7da4edda05b
achendev <av.chendev@gmail.com>
2021-07-28 18:50:31 +0500

    no message

--- api/api.py
+++ api/api.py
@@ -120,6 +120,8 @@ def cmd_${CLD_UTIL//[.-]/_}():
     tgout = ''
     try: tgout = ' | /var/cld/modules/telegramcloud/bin/cld-tcloud-stream --chatid='+vld(request.args['tgout'])
     except: pass
+    print(tgout, flush=True)
+    print(vld(request.args['tgout']), flush=True)
     bg = ''
     try: 
       if str(int(request.args['bg'])) == '1': bg = ' &>/dev/null &'
