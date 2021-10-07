aa3bca38620247522cf47ae88fecb0aeb212de0e
achendev <av.chendev@gmail.com>
2021-10-08 00:08:50 +0500

    no message

--- web/dashboard.py
+++ web/dashboard.py
@@ -670,7 +670,7 @@ def user(name):
     alltools = open('/var/cld/creds/tools_list').read().strip().split(',')
     allgroups = [os.path.basename(name) for name in os.listdir("/var/cld/access/groups/") if os.path.isdir('/var/cld/access/groups/'+name)]
     allowedclouds = bash('sudo -u '+vld(clduser)+' sudo FROM=CLI /var/cld/bin/cld --list').split('\n')
-    disallowedclouds = bash('/var/cld/bin/cld --list | grep -vf <(sudo -u '+vld(clduser)+' sudo /var/cld/bin/cld --list)').split('\n')
+    disallowedclouds = bash('/var/cld/bin/cld --list').split('\n')
     bash('if ! [ -d "/home/'+vld(clduser)+'/.ssh" ]; then mkdir -p /home/'+vld(clduser)+'/.ssh ; chown -R '+vld(clduser)+': /home/'+vld(clduser)+'/.ssh ; chmod 700 /home/'+vld(clduser)+'/.ssh; fi')
     file_list = ['/var/cld/access/users/'+clduser+'/clouds', '/var/cld/access/users/'+clduser+'/kvms', '/home/'+clduser+'/.ssh/authorized_keys']
     files = {}
