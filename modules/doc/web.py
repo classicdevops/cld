01c4b795cc34051630530855c3c698ec24e9014c
achendev <av.chendev@gmail.com>
2021-10-29 01:53:10 +0500

    no message

--- modules/doc/web.py
+++ modules/doc/web.py
@@ -8,6 +8,8 @@ def doc_index():
     checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
     return render_template('modules/doc/doc.html')
+  else:
+    return redirect('/login', code=302)
 
 @app.route("/doc/doc.json")
 def doc_spec():
@@ -18,3 +20,5 @@ def doc_spec():
     if os.path.isfile('/var/cld/modules/doc/data/doc.json') != True:
     	bash('python3 /var/cld/modules/doc/doc.py &>/dev/null')
     return Response(open('/var/cld/modules/doc/data/doc.json', 'r').read(), status=200, mimetype='application/json')
+  else:
+    return redirect('/login', code=302)
