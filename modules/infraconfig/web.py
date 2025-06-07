28d73b2b4613dcee3cfeed5def06ba446c765c43
Aleksandr Chendev <45525349+achendev@users.noreply.github.com>
2025-06-07 04:10:41 +0300

    Codex/implement infraconfig module with web and cli support (#17) (#18)
    * Remove redundant web run endpoint
    
    * Rewrite infraconfig web UI to edit settings (#15)

--- modules/infraconfig/web.py
+++ modules/infraconfig/web.py
@@ -11,7 +11,13 @@ def infraconfig_index():
     user = session['username']
     checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
+    os.makedirs('/var/cld/modules/infraconfig/data', mode = 0o700, exist_ok=True)
+    bash('''[ -d /var/cld/modules/infraconfig/data/groups ] || mkdir -p /var/cld/modules/infraconfig/data/groups ; [ -d /var/cld/modules/infraconfig/data/instances ] || mkdir -p /var/cld/modules/infraconfig/data/instances''')
+    instances_files = path_to_dict('/var/cld/modules/infraconfig/data/instances')
+    groups_files = path_to_dict('/var/cld/modules/infraconfig/data/groups')
+    configs = {"instances": instances_files, "groups": groups_files}
     cld_instances = bash('sudo -u '+vld(user)+' sudo FROM=CLI /var/cld/bin/cld --list --all').split('\n')
     cld_groups = [os.path.basename(name) for name in os.listdir('/var/cld/access/groups/') if os.path.isdir('/var/cld/access/groups/'+name)]
-    return render_template('modules/infraconfig/infraconfig.html', username=user, cld_instances=cld_instances, cld_groups=cld_groups)
+    return render_template('modules/infraconfig/infraconfig.html', username=user, cld_instances=cld_instances, cld_groups=cld_groups, configs=configs)
+
 
