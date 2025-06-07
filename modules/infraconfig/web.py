6fafb936837610743e4f9f549c59e58ad4058d5d
Aleksandr Chendev <45525349+achendev@users.noreply.github.com>
2025-06-07 03:44:56 +0300

    Rewrite infraconfig web UI to edit settings

--- modules/infraconfig/web.py
+++ modules/infraconfig/web.py
@@ -1,6 +1,6 @@
 webmodule["infraconfig"] = {}
 webmodule["infraconfig"]["homename"] = "Infra config"
-webmodule["infraconfig"]["desc"] = "Collect instance configuration"
+webmodule["infraconfig"]["desc"] = "Manage parsing settings"
 
 @app.route("/infraconfig")
 def infraconfig_index():
@@ -11,7 +11,12 @@ def infraconfig_index():
     user = session['username']
     checkresult = checkpermswhiteip(cldmodule, 'NOTOOL', user, remoteaddr())
     if checkresult[0] != "granted": return Response("403", status=403, mimetype='application/json')
+    os.makedirs('/var/cld/modules/infraconfig/data', mode = 0o700, exist_ok=True)
+    bash('''[ -d /var/cld/modules/infraconfig/data/groups ] || mkdir -p /var/cld/modules/infraconfig/data/groups ; [ -d /var/cld/modules/infraconfig/data/instances ] || mkdir -p /var/cld/modules/infraconfig/data/instances''')
+    instances_files = path_to_dict('/var/cld/modules/infraconfig/data/instances')
+    groups_files = path_to_dict('/var/cld/modules/infraconfig/data/groups')
+    configs = {"instances": instances_files, "groups": groups_files}
     cld_instances = bash('sudo -u '+vld(user)+' sudo FROM=CLI /var/cld/bin/cld --list --all').split('\n')
     cld_groups = [os.path.basename(name) for name in os.listdir('/var/cld/access/groups/') if os.path.isdir('/var/cld/access/groups/'+name)]
-    return render_template('modules/infraconfig/infraconfig.html', username=user, cld_instances=cld_instances, cld_groups=cld_groups)
+    return render_template('modules/infraconfig/infraconfig.html', username=user, cld_instances=cld_instances, cld_groups=cld_groups, configs=configs)
 
